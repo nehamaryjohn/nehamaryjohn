82ad6a5ded9e4db8c7e7116f794fac0fbd6aa9a3
nehamaryjohn <nehamary996@gmail.com>
2024-11-27 10:50:19 +0530

    Add files via upload

---
+++ color.py
@@ -0,0 +1,13 @@
+color=input("ENTER COLORS BY COMMA SEPERATOR:")
+color_list1=color.split(',')
+print(color_list1)
+print("First color:",color_list1[0])
+print("Last color:",color_list1[-1])
+color_list2=['Red','Orange','Black','White']
+print("color-list1 not contained in color-list2 are:")
+diff=list(set(color_list1)-set(color_list2))
+print(diff)
+color_int_list=[]
+for color in color_list1:
+    color_int_list.append(len(color))
+print("List of integers corresponding to each color:",color_int_list)
