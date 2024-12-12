e287686639be7eccc3c9f2feae5acbc29fbb9f15
nehamaryjohn <nehamary996@gmail.com>
2024-12-11 21:06:58 -0800

    Add files via upload

---
+++ coloumcsv.py
@@ -0,0 +1,17 @@
+import csv
+file_name = 'car.csv'
+columns_to_read = ['Company','Car Model']
+try:
+    with open(file_name, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        missing_columns = [col for col in columns_to_read if col not in reader.fieldnames]
+        if missing_columns:
+            print(f"Error:Missing columns in the file:{','.join(missing_columns)}")
+        else:
+            print(",".join(columns_to_read))
+            for row in reader:
+                print(",".join(row[col] for col in columns_to_read))
+except FileNotFoundError:
+    print(f"Error:File '{file_name}'not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
